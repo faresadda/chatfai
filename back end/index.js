1c4a3cd09e2153a5fdc6cd948ab7e6933724aa96
faresadda <faresadda2001off@gmail.com>
2025-04-17 23:16:22 +0100

    'commit'

--- back end/index.js
+++ back end/index.js
@@ -16,8 +16,7 @@ app.all('*',(req,res,next)=>{
 app.use(errorMiddleware)
 app.use(apikeyMiddleware)
 
-import path from 'path'
-import { fileURLToPath } from 'url'
+const path = require('path');
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
