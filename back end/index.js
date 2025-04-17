b56abb651b3fca856f8072596f3eade2fb51f3f6
faresadda <faresadda2001off@gmail.com>
2025-04-17 22:22:32 +0100

    'commit'

--- back end/index.js
+++ back end/index.js
@@ -15,6 +15,19 @@ app.all('*',(req,res,next)=>{
     return res.status(404).json(appError.createError(404,'not found'))})
 app.use(errorMiddleware)
 app.use(apikeyMiddleware)
+
+const path = require('path');
+// ... باقي الإعدادات والروترات
+
+// Serve static files from the frontend
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Fallback to index.html for any unmatched route (important for React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+
 app.listen(process.env.PORT,()=>{
     console.log('listenning on port ',process.env.port)
 })
